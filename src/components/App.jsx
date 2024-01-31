ce61a4b38cff976b1bc5572892b53d610b9dec73
TarasKupchenko <133959505+TarasKupchenko@users.noreply.github.com>
2024-01-31 17:24:42 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,11 @@
-//import React, { lazy, Suspense } from 'react';
-//import { Routes, Route, NavLink } from 'react-router-dom';
-import { NavLink } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
-import Home from './Home/Home';
-import MovieDetails from './MovieDetails/MovieDetails';
-import Movies from './pages/Movies';
+import React, { lazy, Suspense } from 'react';
+import { Routes, Route, NavLink } from 'react-router-dom';
 import css from './App.module.css';
+import Loader from './Loader/Loader';
 
-//export const Home = lazy(() => import('./Home/Home'));
-//const Movies = lazy(() => import('./components/Movies'));
-//const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'));
-//const Cast = lazy(() => import('./components/Cast'));
-//const Reviews = lazy(() => import('./components/Reviews'));
+const Home = lazy(() => import('./Home/Home'));
+const Movies = lazy(() => import('./pages/Movies'));
+const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'));
 
 export const App = () => {
   return (
@@ -35,14 +29,14 @@ export const App = () => {
           Movies
         </NavLink>
       </header>
-      <Routes>
-        {/*<Suspense fallback={<div>Loading...</div>}>*/}
-        <Route path="/" element={<Home />} />
-        <Route path="/movies/" element={<Movies />} />
-        <Route path="/movies/:movieId/*" element={<MovieDetails />} />
 
-        {/*</Suspense>*/}
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies/" element={<Movies />} />
+          <Route path="/movies/:movieId/*" element={<MovieDetails />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
