8cf3783038c7646d14bcaa26183228d2c26b4a20
TarasKupchenko <133959505+TarasKupchenko@users.noreply.github.com>
2024-02-01 21:15:12 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import React, { lazy, Suspense } from 'react';
-import { Routes, Route, NavLink } from 'react-router-dom';
-import css from './App.module.css';
+import { Routes, Route } from 'react-router-dom';
 import Loader from './Loader/Loader';
 
+const SharedLayout = lazy(() => import('./SharedLayout/SharedLayout'));
 const Home = lazy(() => import('./Home/Home'));
 const Movies = lazy(() => import('./pages/Movies'));
 const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'));
@@ -10,31 +10,13 @@ const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'));
 export const App = () => {
   return (
     <div>
-      <header>
-        <NavLink
-          className={({ isActive }) =>
-            `${css.navLink} ${isActive ? css.active : ''}`
-          }
-          to="/"
-        >
-          Home
-        </NavLink>
-        |
-        <NavLink
-          className={({ isActive }) =>
-            `${css.navLink} ${isActive ? css.active : ''}`
-          }
-          to="/movies"
-        >
-          Movies
-        </NavLink>
-      </header>
-
       <Suspense fallback={<Loader />}>
         <Routes>
-          <Route index path="/" element={<Home />} />
-          <Route path="/movies/" element={<Movies />} />
-          <Route path="/movies/:movieId/*" element={<MovieDetails />} />
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/movies/" element={<Movies />} />
+            <Route path="/movies/:movieId/*" element={<MovieDetails />} />
+          </Route>
         </Routes>
       </Suspense>
     </div>
---
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+import css from './SharedLayout.module.css';
+
+const SharedLayout = () => {
+  return (
+    <div>
+      <header>
+        <NavLink
+          className={({ isActive }) =>
+            `${css.navLink} ${isActive ? css.active : ''}`
+          }
+          to="/"
+        >
+          Home
+        </NavLink>
+        |
+        <NavLink
+          className={({ isActive }) =>
+            `${css.navLink} ${isActive ? css.active : ''}`
+          }
+          to="/movies"
+        >
+          Movies
+        </NavLink>
+      </header>
+      <Outlet />
+    </div>
+  );
+};
+
+export default SharedLayout;
