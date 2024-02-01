43b2932ab11a15c3d6367683cc9f8b237b7eca9b
TarasKupchenko <133959505+TarasKupchenko@users.noreply.github.com>
2024-02-01 21:21:29 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,8 @@ const SharedLayout = lazy(() => import('./SharedLayout/SharedLayout'));
 const Home = lazy(() => import('./Home/Home'));
 const Movies = lazy(() => import('./pages/Movies'));
 const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'));
+const Reviews = lazy(() => import('./Reviews/Reviews '));
+const Cast = lazy(() => import('./Cast/Cast'));
 
 export const App = () => {
   return (
@@ -15,7 +17,10 @@ export const App = () => {
           <Route path="/" element={<SharedLayout />}>
             <Route index element={<Home />} />
             <Route path="/movies/" element={<Movies />} />
-            <Route path="/movies/:movieId/*" element={<MovieDetails />} />
+            <Route path="/movies/:movieId/*" element={<MovieDetails />}>
+              <Route index element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
           </Route>
         </Routes>
       </Suspense>
